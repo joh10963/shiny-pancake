6af8c51c76e426af2f55990ff2ea0f691d2f52ed
joh10963 <joh10963@umn.edu>
2017-04-11 21:39:47 -0500

    Algorithm verification

--- algorithm.py
+++ algorithm.py
@@ -7,29 +7,36 @@ Created on Fri Feb 17 14:43:49 2017
 
 import numpy as np
 
+import math
+
 import matplotlib.pyplot as plt
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import RMSprop
 
-def create_state(activities, attributes):
-    '''create the state matrix. [activity, attribute]'''
-#    state = np.zeros((4, 5))
-    state = np.zeros((activities, attributes))
+def create_random_state(activities, attributes):
+    '''create the state matrix. [activity, attribute, category]'''
+    state = np.zeros((activities, attributes, 3))
     for i in range(activities):
         for j in range(attributes):
-            if np.random.random() > 0.5:
-                state[i, j] = 1
+                state[i, j, np.random.randint(0, 3)] = 1
     
     return state
+   
+def get_patient_feedback(activites):
+    '''Ask the patient which activity they like better
+    1 or 0 for each activity
+    1: they like it 
+    0: they don't like it
+    '''
+    #they like all the activities
+    output = np.ones((1, activites))
     
-def create_preference(activities):
-    preference = np.zeros((1, activities))
-    for i in range(activities):
-        if np.random.random() > 0.5:
-            preference[0, i] = 1
-    return preference
+    #I'm just gonna say they like the first 10 of the activities
+    #output[0, 0:10] = 1
+    
+    return output
     
 def create_model(activities, inputs):
     model = Sequential()
@@ -47,22 +54,40 @@ def create_model(activities, inputs):
 
     return model
     
-def get_reward(state, action, preference):
-    '''positive reward for action=0 or action==1, negative reward for everything else'''
-    # for now we only care about the first attribute
-    output = 0
-    for i in range(preference.size):
-        if state[action][i] == preference[0, i]:
-            output += 1
-    return output
-#    if state[action][0] == preference[0]:
-#        return 10.0
-#    return 0.0
+def get_simulation_reward_activity(action):
+    '''only accept certain activities'''
+    if action in range(10):
+        return 1.0
+    else:
+        return -1.0
 
-def train_model(model, state, preference):
+def get_simulation_reward_attribute(action, state):
+    '''Only accept Attribute 3 100'''
+    if state[action, 2, 0] == 1:
+        return 1.0
+    else:
+        return -1.0
+
+def get_training_reward(action, feedback, state):
+    '''try to simulate a real-life person'''
+    if feedback[0, action] == 1: #this means they said they liked the activity
+    #if state[action, 2, 0] == 1:
+        prob = np.random.normal(0.6, 0.2)
+    else:
+        prob = np.random.normal(0.4, 0.2)
+    
+    #use the probability to simulate if they pick the activity or not
+    stop = int(prob*100.0)
+    draw = np.zeros((1, 100))
+    draw[0, 0:stop] = 1
+    pick = draw[0, np.random.randint(0, 100)]
+    if pick == 0:
+        return -1.0
+    if pick == 1:
+        return 1.0
+
+def train_model(model, state, feedback, epochs=1):
     '''train the model, preference is a 1x4 vector indicating the preference of what will be selected'''
-
-    epochs = 50
     
     inputs = state.size
     activities = state.shape[0]
@@ -70,9 +95,9 @@ def train_model(model, state, preference):
     suggested = 0
     accepted = 0
     
-    gamma = 0.25
+    gamma = 0.4
     epsilon = 1
-    threshold = 0.75
+    threshold = 0.5
     
     for i in range(epochs):
         
@@ -87,7 +112,7 @@ def train_model(model, state, preference):
             else:
                 action = np.argmax(qval)
                 
-            reward = get_reward(state, action, preference)
+            reward = get_training_reward(action, feedback, state)
             
             #update the matrices of the saved values
             suggested += 1
@@ -107,40 +132,44 @@ def train_model(model, state, preference):
             if epsilon > 0.1:
                 epsilon -= 0.001
 
-            if accepted > 10 and accepted/float(suggested) > threshold:
+            if accepted > activities and accepted/float(suggested) > threshold or suggested > 1000:
                 done = True
-                print accepted/float(suggested)
-                print action
-                print 'done'
-                print 'number of iterations = ' + str(suggested)
+#                print accepted/float(suggested)
+#                print action
+#                print 'done'
+#                print 'number of iterations = ' + str(suggested)
     return model
         
-def run_simulation(model, state, preference):
+def run_simulation(model, state):
     
     #create arrays to save the suggested activity and the accepted activity
     epochs = 50
     suggested = np.zeros((1, epochs))
     accepted = 0
+    accepted_matrix = np.zeros((1, epochs))
     inputs = state.size
     activities = state.shape[0]
     
-    gamma = 0.25
-    last_action = -1
+    gamma = 0.4
+#    last_action = -1
 
     for i in range(epochs):
         qval = model.predict(state.reshape(1, inputs), batch_size=1)
         action = np.argmax(qval)
-        if action == last_action:
-            q = np.array(qval, copy=True)
-            q[0, action] = 0
-            action = np.argmax(q)
-        last_action = action
-        reward = get_reward(state, action, preference)
+#        if action == last_action:
+#            q = np.array(qval, copy=True)
+#            q[0, action] = 0
+#            action = np.argmax(q)
+#        last_action = action
+        
+        reward = get_simulation_reward_activity(action)
+        #reward = get_simulation_reward_attribute(action, state)
         
         #update the matrices of the saved values
         suggested[0, i] = action
         if reward > 0.0:
             accepted += 1
+            accepted_matrix[0, i] = 1
         
         newQ = model.predict(state.reshape(1,inputs), batch_size=1)
         maxQ = np.argmax(newQ)
@@ -152,15 +181,8 @@ def run_simulation(model, state, preference):
         y[0][action] = update
         model.fit(state.reshape(1,inputs), y, batch_size=1, nb_epoch=1, verbose=0)
             
-    return accepted, suggested
+    return suggested, accepted_matrix
     
-def suggest_activity(state, action):
-    '''action is the state to suggest, right now it doesn't change the state'''    
-#    if patient_accepts(action):
-#        state[0][action] = state[0][action] + 1 #update the frequency
-#    state[1][action] = state[1][action] + 1 #update the availability
-    
-    return state
 
 def patient_accepts(action):
     #This will only accept action=0 or action=1
@@ -170,55 +192,152 @@ def patient_accepts(action):
     
 
 
-def rand_simulate():
+def random_simulation(activites):
+    '''only accepts activity 3 and 6'''
+    suggested_matrix = np.random.randint(0, activites, size=(1,50))
+    accepted_matrix = np.zeros(suggested_matrix.shape)
     
-    state = create_state()
-    rand_suggestions = np.zeros((1, 10))
+    for i in range(suggested_matrix.size):
+        if suggested_matrix[0, i] == 10:
+            accepted_matrix[0, i] = 1
     
-    for i in range(rand_suggestions.size):
-        rand_action = np.random.randint(0, 5)
-        state = suggest_activity(state, rand_action)
-        rand_suggestions[0][i] = rand_action
-        
-    print rand_suggestions
-    print state
+    return suggested_matrix, accepted_matrix
 
+def random_simulation_attributes(state, activities):
+    '''only accepts attribute 2 100'''
+    suggested_matrix = np.random.randint(0, activities, size=(1,50))
+    accepted_matrix = np.zeros(suggested_matrix.shape)
     
+    for i in range(suggested_matrix.size):
+        suggested_activity = suggested_matrix[0, i]
+        if  state[suggested_activity, 2, 0] == 1:
+            accepted_matrix[0, i] = 1
+    
+    return suggested_matrix, accepted_matrix
+
+def window(suggested, accepted, window_length=10.0):
+    windows = int(math.ceil(suggested.size/window_length))
+    output = np.zeros((1, windows))
+    
+    for i in range(windows):
+        j = 0
+        while j < window_length and i*window_length+j < suggested.size:
+            if accepted[0, int(i*window_length+j)] == 1:
+                output[0, i] += 1
+            j += 1
+        output[0, i] = output[0, i]/float(j)
+
+    return output            
 
 
-def plot_results(suggested_matrix):
+def plot_results(suggested_matrix, accepted_matrix, activities, attributes=False, state=None):
     plt.figure()
     
-    time = np.zeros((1, suggested_matrix.size))
+    time = np.arange(0, suggested_matrix.size)
+    
     for i in range(time.size):
-        time[0, i] = i
-        
-    plt.plot(time, suggested_matrix, 'o-')
+        if accepted_matrix[0, i]:
+            plt.plot(time[i], suggested_matrix[0, i], 'o', color='green')
+        else:
+            plt.plot(time[i], suggested_matrix[0, i], 'o', color='red')
     
     plt.xlabel('Iteration')
     plt.ylabel('Activity')
+    plt.ylim([0, activities])
+    
+    if attributes:
+        #draw a horizontal line for the accepted attributes
+        for act in range(activities):
+            if state[act, 2, 0] == 1: #accepted activity
+                plt.plot([0, 50], [act, act], '-', color='blue')
     
     plt.show()
     
+def main2():
+    activities = 20
+    attributes = 5
+    state = create_random_state(activities, attributes)
+    feedback = get_patient_feedback(activities)
 
-def main():
-    activities = 10
-    attributes = 10
-    #make up initial data
-    preference = create_preference(activities)
-    state = create_state(activities, attributes)
-    print state
-    print preference
-    #create the neural network
-    model = create_model(state.shape[0], state.size)
-    #train the network
-    model = train_model(model, state, preference)
-    #run simulation
-    accepted, suggested = run_simulation(model, state, preference)
-    print np.sum(accepted)/float(50)
-    plot_results(suggested)
-    #plot the results
+    #Random simulation
+#    suggested, accepted = random_simulation(activities)
+#    plot_results(suggested, accepted, activities)
+#    
+#    windowed_result = np.zeros((50, 10))    
+#    
+#    for i in range(50):
+#        print 'starting round ' + str(i)
+#        suggested, accepted = random_simulation(activities)
+#        #plot_results(suggested, accepted, activities)
+#        output = window(suggested, accepted, 5.0)
+#        windowed_result[i] = output
+#    
+#    plt.figure()
+#    time = np.arange(0, windowed_result.shape[1])
+#    avg_window = np.mean(windowed_result, axis=0)
+#    plt.plot(time, avg_window, 'o', color='blue')
+#    plt.xlabel('Window')
+#    plt.ylabel('Percent Accepted')
+#    plt.ylim([0, 1])
+#    plt.show()
+    
+    #Show a random simulation
+#    suggested, accepted = random_simulation_attributes(state, activities)
+#    plot_results(suggested, accepted, activities, attributes=True, state=state)
+        
+#    windowed_result = np.zeros((50, 10))    
+#    
+#    for i in range(50):
+#        print 'starting round ' + str(i)
+#        suggested, accepted = random_simulation_attributes(state, activities)
+#        #plot_results(suggested, accepted, activities)
+#        output = window(suggested, accepted, 5.0)
+#        windowed_result[i] = output
+#    
+#    plt.figure()
+#    time = np.arange(0, windowed_result.shape[1])
+#    avg_window = np.mean(windowed_result, axis=0)
+#    plt.plot(time, avg_window, 'o', color='blue')
+#    plt.xlabel('Window')
+#    plt.ylabel('Percent Accepted')
+#    plt.ylim([0, 1])
+#    plt.show()
+
+    #Test that the training is appropriate
+
+    #Without training
+#    model = create_model(state.shape[0], state.size)
+#    suggested, accepted = run_simulation(model, state)
+#    plot_results(suggested, accepted, activities)
+    
+    #with training
+#    model = create_model(state.shape[0], state.size)
+#    train_model(model, state, feedback)
+#    suggested, accepted = run_simulation(model, state)
+#    plot_results(suggested, accepted, activities)
+
+    #Show that the predictions get better over time
+    windowed_result = np.zeros((50, 10))    
+    
+    for i in range(50):
+        print 'starting round ' + str(i)
+        model = create_model(state.shape[0], state.size)
+        train_model(model, state, feedback)
+        suggested, accepted = run_simulation(model, state)
+        #plot_results(suggested, accepted, activities)
+        output = window(suggested, accepted, 5.0)
+        windowed_result[i] = output
+    
+    plt.figure()
+    time = np.arange(0, windowed_result.shape[1])
+    avg_window = np.mean(windowed_result, axis=0)
+    plt.plot(time, avg_window, 'o', color='blue')
+    plt.xlabel('Window')
+    plt.ylabel('Percent Accepted')
+    plt.ylim([0, 1])
+    plt.show()
+    
     
 if __name__=='__main__':
-    main()
+    main2()
         
